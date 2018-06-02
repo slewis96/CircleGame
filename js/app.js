0ca9741809bea5a8f6d2ccee14d88fed2e2dc5ea
slewis96 <seblewis96@gmail.com>
2018-06-02 14:23:03 +0100

    added menu and difficulty options

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+//INITIALISE
 $(document).ready(function(){
+  //game
   $('#circle').click(function(){
     event.stopPropagation();
     dotClick();
@@ -7,7 +9,43 @@ $(document).ready(function(){
     event.stopPropagation();
     gameOver();
   })
+
+  //menu
+    //onclicks
+  $('#settings').click(function(){
+    openMenu();
+  });
+  $('#settingsModal').click(function(){
+    $('#settingsModal').css('display', 'none');
+  });
+  $('.innerModal').click(function(){
+    event.stopPropagation();
+  });
+  $.each($('.diffBtns'), function(){
+    $(this).click(function(){
+      $('.diffBtns').removeClass('active');
+      $(this).addClass('active');
+      setSliders($(this).attr('id'));
+      setTimeout(setDifficulty(sizeSlider.value, timerSlider.value), 2);
+    });
+  });
+  //set slider val
+  var sizeSlider = document.getElementById('sizeSlider');
+  var timerSlider = document.getElementById('timerSlider');
+  $('#sizeValue').html(sizeSlider.value);
+  $('#timerValue').html(timerSlider.value);
+  sizeSlider.oninput = function() {
+    $('#sizeValue').html(sizeSlider.value);
+  }
+  timerSlider.oninput = function() {
+    $('#timerValue').html(timerSlider.value);
+  }
+  $('#confirm').click(function(){
+    setDifficulty(sizeSlider.value, timerSlider.value);
+  });
+
 });
+//VARIABLES
 var clicks = 0;
 var score = 0;
 var timer;
@@ -19,6 +57,7 @@ var difficulty = {
 var diffToggle = true;
 var fadeToggle = true;
 
+//FUNCTIONS
 function dotClick(){
   clearInterval(timer);
   var maxwidth = $('.background').width()-200;
@@ -35,7 +74,7 @@ function dotClick(){
   });
   fadeOut();
   clicks++;
-  setDifficulty();
+  increaseDifficulty();
   setScore(difficulty.scrMult);
   timer = setInterval(gameOver, difficulty.time);
 }
@@ -48,18 +87,49 @@ function fadeOut(){
     });
   }
 }
-function setDifficulty(){
+function increaseDifficulty(){
     if(diffToggle){
-      difficulty.size = 100-(clicks/2);
-      difficulty.time = 2000-(clicks*10);
-      difficulty.scrMult = Math.floor(clicks/2);
+      difficulty.size = difficulty.size-0.5;
+      difficulty.time = difficulty.time-15;
     }
+    difficulty.scrMult = Math.floor(clicks/2);
+}
+function setDifficulty(size, time){
+  difficulty.size = size;
+  difficulty.time = time;
 }
 function setScore(x){
   score += x;
-  $('#score').html("Score: " + score);
+  $('#score').html("Score: " + score +",  Clicks: " + clicks);
 }
 function gameOver(){
   alert("Game Over!");
   location.reload();
+}
+function openMenu(){
+  $('#settingsModal').css('display', 'block');
+}
+function setSliders(diff){
+  var sizeSlider = document.getElementById('sizeSlider');
+  var timerSlider = document.getElementById('timerSlider');
+  switch(diff){
+    case "easy":
+      sizeSlider.value = 150;
+      timerSlider.value = 3000;
+      $('#sizeValue').html(sizeSlider.value);
+      $('#timerValue').html(timerSlider.value);
+      break;
+    case "medium":
+      sizeSlider.value = 100;
+      timerSlider.value = 2000;
+      $('#sizeValue').html(sizeSlider.value);
+      $('#timerValue').html(timerSlider.value);
+      break;
+    case "hard":
+      sizeSlider.value = 50;
+      timerSlider.value = 1000;
+      $('#sizeValue').html(sizeSlider.value);
+      $('#timerValue').html(timerSlider.value);
+      break;
+  }
 }
