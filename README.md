16bc8fc6c369461a716b0ce2bb93027f62ba615b
slewis96 <seblewis96@gmail.com>
2018-06-03 16:47:01 +0100

    added plan swapped over to div instead of svg

---
+++ README.md
@@ -0,0 +1,24 @@
+# Circle Game
+Click on the circle as soon as it appears miss then **gameover** don't click in time then **gameover**
+More successful clicks means a smaller target and a shorter time to click
+
+## Issues:
+- [x] Circle goes off screen
+- [x] Misclick not working
+
+## Todo:
+- [x] setInterval dot change (setInterval bottom of move func, clearInterval at top) if function with interval runs gameOver
+- [x] After x amount of clicks shrink circle, set interval time smaller, change score multiplyer
+- [ ] Give instructions through modal then Ajax to refresh frame on gameover
+- [ ] Disable Misclick
+- [ ] Swap functions to not include setInterval on toggle button for timed
+- [ ] Toggle button for "mania" untimed, unlimited amount of circles, test accuracy (limited amount of circles)
+### Todo extra:
+- [ ] Sliders for initial interval time and size
+- [ ] Input color for circle and background
+
+### Notes:
+Modal instructions & settings
+Main color hex: #c20000
+Font: <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+https://fonts.google.com/?selection.family=PT+Sans
--- js/app.js
+++ js/app.js
@@ -1,15 +1,45 @@
+$(document).ready(function(){
+  $('#circle').click(function(){
+    event.stopPropagation();
+    dotClick();
+  })
+  $('.background').click(function(){
+    event.stopPropagation();
+    gameOver();
+  })
+});
 var clicks = 0;
 var score = 0;
+var timer;
+var difficulty = {
+  size: 100,
+  time: 2000,
+  scrMult: 1
+};
 
 function dotClick(){
-  var maxwidth = $('#gameFrame').width()-50;
-  var maxheight = $('#gameFrame').height()-50;
-  var circle = document.getElementById("circle");
-  circle.setAttribute("cx", (Math.floor((Math.random() * maxwidth) + 50)));
-  circle.setAttribute("cy", (Math.floor((Math.random() * maxheight) + 50)));
+  clearInterval(timer);
+  var maxwidth = $('.background').width()-200;
+  var maxheight = $('.background').height()-200;
+  $('#circle').css("left", (Math.floor(Math.random() * maxwidth)));
+  $('#circle').css("top", (Math.floor(Math.random() * maxheight)));
+  $('#circle').css("width", difficulty.size);
+  $('#circle').css("height", difficulty.size);
   clicks++;
+  setDifficulty();
+  setScore(difficulty.scrMult);
+  timer = setInterval(gameOver, difficulty.time);
+}
+function setDifficulty(){
+    difficulty.size = 100-(clicks/2);
+    difficulty.time = 2000-(clicks*12);
+    difficulty.scrMult = Math.floor(clicks/2);
+}
+function setScore(x){
+  score += x;
+  $('#score').html("Score: " + score);
 }
 function gameOver(){
-  alert("Game Over!")
+  alert("Game Over!");
   location.reload();
 }
